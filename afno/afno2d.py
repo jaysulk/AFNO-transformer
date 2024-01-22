36077fb0e143d45474308d37d4bf51b7a57d6f86
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-01-21 19:24:50 -0800

    Update afno2d.py

--- afno/afno2d.py
+++ afno/afno2d.py
@@ -48,17 +48,24 @@ class AFNO2D(nn.Module):
 
     def forward(self, x):
         bias = x
+
         dtype = x.dtype
         x = x.float()
         B, N, C = x.shape
-        H = W = int(math.sqrt(N))
+
+        if spatial_size == None:
+            H = W = int(math.sqrt(N))
+        else:
+            H, W = spatial_size
 
         x = x.reshape(B, H, W, C)
         x = dht2d(x)
-        x = x.reshape(B, H, W, self.num_blocks, self.block_size)
+        x = x.reshape(B, x.shape[1], x.shape[2], self.num_blocks, self.block_size)
 
         o1 = torch.zeros([B, H, W, self.num_blocks, self.block_size * self.hidden_size_factor], device=x.device)
         o2 = torch.zeros(x.shape, device=x.device)
+        #o2_real = torch.zeros(x.shape, device=x.device)
+        #o2_imag = torch.zeros(x.shape, device=x.device)
 
         total_modes = H * W
         kept_modes = int(math.sqrt(total_modes) * self.hard_thresholding_fraction)
