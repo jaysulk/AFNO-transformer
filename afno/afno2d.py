a7c4f1dc50575ede985e1b0cc86148a47deb65bc
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2024-01-25 19:13:34 -0800

    Update afno2d.py

--- afno/afno2d.py
+++ afno/afno2d.py
@@ -35,7 +35,7 @@ def convolution_multiply2d(x, y):
     Yplus = Y + Yflip
     Yminus = Y - Yflip
 
-    Z = 0.5 * (torch.einsum("bchw,bchw->bchw", X, Yplus) + torch.einsum("bchw,bchw->bchw", Xflip, Yminus))
+    Z = 0.5 * (torch.einsum("bcwhd,abcde->awhde", X, Yplus) + torch.einsum("bcwhd,abcde->awhde", Xflip, Yminus))
     z = idht2d(Z)
     return z
 
